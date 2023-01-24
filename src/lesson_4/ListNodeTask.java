b0af1216b0e174acd8805f865ad7e61cd4df50d6
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2023-01-24 23:40:47 +0300

    Completed homework for the third and fourth seminars.

---
+++ src/lesson_4/ListNodeTask.java
@@ -0,0 +1,44 @@
+package lesson_4;
+
+public class ListNodeTask {
+    public static ListNode addFirst(int value, ListNode node) {
+        ListNode newNode = new ListNode();
+        newNode.val = value;
+        if (node != null) {
+            newNode.next = node;
+        }
+        return newNode;
+    }
+    public static ListNode removeFirst(ListNode remove) {
+        if (remove != null) {
+            remove = remove.next;
+        }
+        return remove;
+    }
+    public static ListNode reverse(ListNode reverse) {
+        ListNode previous = null;
+        while (reverse != null) {
+            ListNode nextElement = reverse.next;
+            reverse.next = previous;
+            previous = reverse;
+            reverse = nextElement;
+        }
+        return previous;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
